004351ec3c4f2f2e41aef877a8da7cb34aa58586
Davi <davicaporal123@hotmail.com>
2022-03-17 21:41:50 -0300

    moved from ytdl to playdl
    ytdl has weird connection aborted errors

--- Commands/play.js
+++ Commands/play.js
@@ -6,8 +6,8 @@ const {
   VoiceChannel
 } = require('discord.js');
 const Voice = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
 const ytsr = require('ytsr');
+const playdl = require('play-dl');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,7 +21,7 @@ module.exports = {
     function validURL(str) {
       var pattern = new RegExp('^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$');
       return !!pattern.test(str);
-    }
+    };
 
     client = interaction.client;
     await interaction.deferReply(); //Differ the reply to avoid errors
@@ -32,10 +32,10 @@ module.exports = {
         ephemeral: true
       });
       return;
-    }
+    };
 
     let args = interaction.options.get('musica').value;
-    if (serverQueue.get('construct') == undefined) {
+    if (typeof serverQueue.get('construct') === 'undefined' || serverQueue.get('construct') === null) {
       const constructTemplate = {
         textChannel: interaction.channelId,
         voiceChannel: interaction.member.voice.channelId,
@@ -45,7 +45,7 @@ module.exports = {
         playing: false
       };
       serverQueue.set('construct', constructTemplate);
-    }
+    };
 
     var songurl;
     if (validURL(args) && args.includes('watch')) { //Video
@@ -56,9 +56,14 @@ module.exports = {
         ephemeral: true
       });
     } else { //Search term
-      searchResult = await ytsr(args, options = {
-        limit: 2
-      });
+      try {
+        searchResult = await ytsr(args, options = {
+          limit: 2
+        });
+      } catch (e) {
+        console.log(e);
+        return;
+      };
 
       fsturl = searchResult.items[0].url.toString();
       sndurl = searchResult.items[1].url.toString();
@@ -73,41 +78,34 @@ module.exports = {
           ephemeral: true
         });
         return;
-      }
+      };
     };
 
     //Info
-    songInfo = await ytdl.getInfo(songurl);
-    let thumb = await songInfo.player_response.videoDetails.thumbnail.thumbnails;
+    songInfo = await playdl.video_basic_info(songurl);
 
-    let likes = await songInfo.videoDetails.likes;
+    let thumb = await songInfo.video_details.thumbnails;
+
+    let likes = await songInfo.video_details.likes;
     if (likes == null) {
       likes = '0'
     };
 
-    let lengthSec = await songInfo.videoDetails.lengthSeconds;
-    lengthSec = parseInt(lengthSec);
-
-    let minutes = Math.floor(lengthSec / 60);
-    let seconds = lengthSec - minutes * 60;
-    if (seconds.toString().length == 1) {
-      seconds = '0' + seconds.toString();
-    };
+    let length = await songInfo.video_details.durationRaw;
 
     let requester = interaction.member.displayName;
 
-    let views = songInfo.player_response.videoDetails.viewCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //Adds commas
+    let views = songInfo.video_details.views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //Adds commas
 
-    let lengthForm = `${minutes}:${seconds}`;
     song = {
-      title: songInfo.videoDetails.title,
-      url: songInfo.videoDetails.video_url,
+      title: songInfo.video_details.title,
+      url: songInfo.video_details.url,
       thumb: thumb[3].url,
       views: views,
-      author: songInfo.player_response.videoDetails.author,
+      author: songInfo.video_details.channel.name,
       likes: likes,
       requester: requester,
-      duration: lengthForm,
+      duration: length,
     };
 
     //Push
@@ -119,20 +117,18 @@ module.exports = {
     creator = voiceChannel.guild.voiceAdapterCreator;
     queueConstruct = await serverQueue.get('construct');
 
-    try {
-      let con = await Voice.getVoiceConnection(interaction.guild.id); //Check if bot is already in a voice channel
+    let con = await Voice.getVoiceConnection(interaction.guild.id); //Check if bot is already in a voice channel
 
-      if (con == undefined) { //If not join voice channel and begin playing
-        const connection = await Voice.joinVoiceChannel({
-          channelId: voiceChannel.id,
-          guildId: guildID,
-          adapterCreator: creator,
-        });
-        queueConstruct.connection = connection;
-
-        play(interaction.guild, queueConstruct.songs[0], true, connection);
-        return;
-      } else { //Else, notify that the song has been added to the queue
+    if (typeof con === 'undefined' || con === null) { //If not join voice channel and begin playing
+      const connection = await Voice.joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: guildID,
+        adapterCreator: creator,
+      });
+      play(interaction.guild, queueConstruct.songs[0], true, connection);
+      return;
+    } else { //Else, notify that the song has been added to the queue
+      if (typeof leaveTimeout === 'undefined' || leaveTimeout === null) {
         queueConstruct.connection = con;
 
         let embed = new MessageEmbed()
@@ -150,12 +146,12 @@ module.exports = {
           .setTimestamp(interaction.createdTimestamp);
         interaction.editReply({
           embeds: [embed]
-        })
+        });
+      } else {
+        queueConstruct.connection = con;
+        play(interaction.guild, queueConstruct.songs[0], true, queueConstruct.connection);
       }
-    } catch (err) {
-      console.log(err)
-      queue.delete(interaction.guild.id);
-    }
+    };
 
     //Play function
     async function play(guild, song, message, connection, player) {
@@ -175,18 +171,21 @@ module.exports = {
           .setTimestamp(interaction.createdTimestamp)
         interaction.editReply({
           embeds: [embed]
-        })
-      }
+        });
+      };
 
       if (!song) {
-        let con = await Voice.getVoiceConnection(interaction.guild.id);
-        con.destroy();
-        queue.delete(guild.id);
+        queueConstruct.playing = false
+        if (typeof leaveTimeout === 'undefined' || leaveTimeout === null) {
+          leaveTimeout = setTimeout(function() {
+            leaveIfInactive()
+          }, 60000);
+        };
         return;
-      }
+      };
 
       var plr;
-      if (player == undefined) {
+      if (typeof player === 'undefined' || player === null) {
         //Create player
         const player = await Voice.createAudioPlayer({
           behaviors: {
@@ -196,37 +195,54 @@ module.exports = {
         plr = player
       } else {
         plr = player
-      }
+      };
 
       //Set resource and play
-      const resource = await Voice.createAudioResource(ytdl(song.url));
+      let songst = await playdl.stream(song.url);
+      const resource = await Voice.createAudioResource(songst.stream, {
+        inputType: songst.type
+      });
       plr.play(resource);
 
       //Add a listener to play the next song in the queue (if it does not exist already)
       if (plr.listenerCount('stateChange') == 0) { //Prevents multiple listeners
         plr.addListener('stateChange', async (oldS, newS) => {
           if (newS.status == 'idle' && oldS.status == 'playing') { //Check if a song finished
-            serverQueue = await queue.get(interaction.guild.id)
-            queueConstruct = await serverQueue.get('construct') //Update the construct
+            serverQueue = await queue.get(interaction.guild.id);
+            queueConstruct = await serverQueue.get('construct'); //Update the construct
 
-            if (!queueConstruct || queueConstruct.songs.length == 0) { //Destroy the connection if there's no more songs
-              let con = await Voice.getVoiceConnection(interaction.guild.id);
-              con.destroy();
-              queue.delete(guild.id);
+            if (!queueConstruct || queueConstruct.songs.length == 0) { //Leave if there's no more songs
+              queueConstruct.playing = false
+              if (typeof leaveTimeout === 'undefined' || variable === null) {
+                leaveTimeout = setTimeout(function() {
+                  leaveIfInactive()
+                }, 60000);
+              };
               return;
-            }
+            };
             if (queueConstruct.songs.length != 0) { //Shift the queue and play the next song
               queueConstruct.songs.shift();
-              await play(guild, queueConstruct.songs[0], false, connection, plr);
-            }
-          }
+              await play(interaction.guild, queueConstruct.songs[0], false, connection, plr);
+            };
+          };
         });
-      }
+      };
 
       //Subscribe
       const subscription = connection.subscribe(plr);
-      serverQueue = queue.get(interaction.guild.id)
-      serverQueue.set('construct', queueConstruct) //Update the serverQueue
-    }
+      queueConstruct.playing = true
+      serverQueue = queue.get(interaction.guild.id);
+      serverQueue.set('construct', queueConstruct); //Update the serverQueue
+    };
+
+    async function leaveIfInactive() {
+      serverQueue = await queue.get(interaction.guild.id);
+      queueConstruct = await serverQueue.get('construct');
+      if (!queueConstruct || queueConstruct.songs.length == 0) {
+        let con = await Voice.getVoiceConnection(interaction.guild.id);
+        con.destroy();
+        queue.delete(interaction.guild.id);
+      };
+    };
   }
 };
