6b916de380a2eaea5f1df477b8795e5398720d62
Davi <davicaporal123@hotmail.com>
2022-08-02 19:23:58 -0300

    fixed ping

--- Commands/ping.js
+++ Commands/ping.js
@@ -8,14 +8,14 @@ module.exports = {
     .setName('ping')
     .setDescription('Testa o seu ping com o bot e mostra a latência com a API.'),
   async execute(interaction) {
-    let ping = (Date.now() - interaction.createdTimestamp)
-    let api = interaction.client.ws.ping
+    let ping = (Date.now() - interaction.createdTimestamp);
+    let api = interaction.client.ws.ping;
     let embed = new EmbedBuilder()
       .setAuthor({
         name: 'Pong!',
         iconURL: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/ping-pong_1f3d3.png'
       })
-      .setColor('GREEN')
+      .setColor('Green')
       .setDescription(`💻 Sua latência com o bot é: \`${ping}ms\`. \n 📡 A API está com \`${api}ms\` de latência.`)
       .setTimestamp(interaction.createdTimestamp)
     await interaction.reply({
